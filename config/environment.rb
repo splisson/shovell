db49a7051f7c6e800d0868ff2449de25701f8e39
Sebastien Plisson <splisson@veodia.com>
2009-10-07 09:52:54 -0700

    try to fix gem version issue on engineyard

--- config/environment.rb
+++ config/environment.rb
@@ -5,7 +5,9 @@
 # ENV['RAILS_ENV'] ||= 'production'
 
 # Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '1.2.1' unless defined? RAILS_GEM_VERSION
+#RAILS_GEM_VERSION = '1.2.1' unless defined? RAILS_GEM_VERSION
+# Tempo workaround engineyard rubygem not at version high enough for ruby 2.3.4
+RAILS_GEM_VERSION = '2.2.3' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
